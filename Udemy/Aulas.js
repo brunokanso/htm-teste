aeda0e058abe89351651c2ba2654cfc50c093383
brunokanso <brunolkanso@gmail.com>
2024-02-10 23:16:18 -0400

    JS

--- Udemy/Aulas.js
+++ Udemy/Aulas.js
@@ -29,19 +29,44 @@
 
 // Arrays
 
-const valores =  [2.3, 3.5, 4.6, 5.7, 6.2 ,9.2]
-console.log(valores)
+// const valores =  [2.3, 3.5, 4.6, 5.7, 6.2 ,9.2]
+// console.log(valores)
 
-valores [6] = 20
+// valores [6] = 20
 
-console.log(valores)
-console.log(valores.length)
-valores.push({id:3}, false , null, 'teste')
-console.log(valores)
+// console.log(valores)
+// console.log(valores.length)
+// valores.push({id:3}, false , null, 'teste')
+// console.log(valores)
 
-console.log(valores.pop())
-console.log(valores)
+// console.log(valores.pop())
+// console.log(valores)
 
-delete valores[0]
-console.log(valores)
+// delete valores[0]
+// console.log(valores)
 
+
+// Objeto
+
+// const prod1 = {}
+// prod1.nome = 'celular'
+// prod1.preco = 4000
+// console.log(prod1)
+
+// const prod2 = {
+//     nome: 'Camisa Men Top',
+//     preco: 90.00,
+//     cor:  'Preta',
+//     tamanho: 'GG',
+// }
+
+// console.log(prod1, prod2)
+// console.log(prod2.nome.length)
+
+// nullUndefined
+
+let valor 
+console.log(valor)
+
+valor = null
+console.log(valor)
