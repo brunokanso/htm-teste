560d7cc7a30f37d0b17b91ca42496bb6d9b764ee
brunokanso <brunolkanso@gmail.com>
2024-02-11 08:54:09 -0400

    JS

--- Udemy/Aulas.js
+++ Udemy/Aulas.js
@@ -63,10 +63,68 @@
 // console.log(prod1, prod2)
 // console.log(prod2.nome.length)
 
+
+
 // nullUndefined
 
-let valor 
-console.log(valor)
+// let valor 
+// console.log(valor)
 
-valor = null
-console.log(valor)
+// valor = null
+// console.log(valor)
+
+
+// Function
+
+// function calculadora(a,b){
+//     if(a + b > 0){
+//       return a + b   
+//     }
+//     else{
+//         return console.log("Numeros digitados não são validos")
+//     }
+// }
+
+// console.log(calculadora(5,6))
+
+
+// const imprimirSoma = function (a, b){
+//     console.log(a + b)
+// }
+// imprimirSoma(2, 3)
+
+
+// Função Arrow
+
+// const soma = (a, b) => {
+//     return a + b
+// }
+// console.log(soma(2,3))
+
+// // Apenas quando a function vai executar apenas uma função
+
+// const subtracao = (a, b) => a - b 
+// console.log(subtracao (2,3))
+
+// const imprimir = a => console.log(a)
+
+// imprimir('Legal carai')
+
+
+// var vs let
+
+var numero = 1 
+{
+    var numero = 2
+    console.log('Dentro do bloco',numero)
+}
+console.log('Fora do Bloco', numero)
+
+
+var numero2 = 1 
+{
+    let numero2 = 2
+    console.log('Dentro do bloco',numero2)
+}
+console.log('Fora do Bloco', numero2)   
+
