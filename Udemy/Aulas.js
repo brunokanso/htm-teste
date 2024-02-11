344cd8629a9bbff5c845d4b3b50808990c49adfd
brunokanso <brunolkanso@gmail.com>
2024-02-10 21:50:02 -0400

    JS

---
+++ Udemy/Aulas.js
@@ -0,0 +1,16 @@
+// Template String para quebrar linha, dar espaço ou concatenar STRING
+
+// const nome = "bruno"
+// const template = `teste 
+// ${nome} !`
+
+// console.log(template)
+
+// console.log(`1 + 1 =  ${1 + 1}`)
+
+
+const up = texto => texto.toUpperCase()
+
+console.log(
+    `Ei... ${up('cuidado')}!`    
+)
