243a9ea01282395c0eb559a08db24be4e61c7a00
brunokanso <brunolkanso@gmail.com>
2024-02-14 16:24:53 -0400

    JS

--- Udemy/Aulas.js
+++ Udemy/Aulas.js
@@ -289,7 +289,7 @@
 //                 CEP: '70080-115'        
 //             } 
 
-<<<<<<< HEAD
+
 // }
 
 // const {nome,idade} = pessoa
@@ -351,28 +351,28 @@
 //OPERADORES RELACIONAIS
 
 
-console.log('01)', '1' == 1)
-console.log('02)', '1' === 1)
-console.log('03)', '3' != 3)
-console.log('04)', '3' !== 3)
-console.log('05)', 3 > 2)
-console.log('06)', 3 < 2)
-console.log('07)', 3<=2)
-console.log('08)', 3>=2)
+// console.log('01)', '1' == 1)
+// console.log('02)', '1' === 1)
+// console.log('03)', '3' != 3)
+// console.log('04)', '3' !== 3)
+// console.log('05)', 3 > 2)
+// console.log('06)', 3 < 2)
+// console.log('07)', 3<=2)
+// console.log('08)', 3>=2)
 
 
-const d1 = new Date(0)
-const d2 = new Date(0)
-console.log('09)', d1 === d2)
-console.log('10)', d1 == d2)
-console.log('11)', d1.getTime() === d2.getTime())
+// const d1 = new Date(0)
+// const d2 = new Date(0)
+// console.log('09)', d1 === d2)
+// console.log('10)', d1 == d2)
+// console.log('11)', d1.getTime() === d2.getTime())
 
 
-console.log('12)', undefined == null)
-console.log('12)', undefined === null)
+// console.log('12)', undefined == null)
+// console.log('12)', undefined === null)
 
 
-=======
+
 // const pessoa = {
 //     nome: 'Ana',
 //     idade: 23, 
@@ -438,4 +438,3 @@ console.log('12)', undefined === null)
 
 
 // OPERADORES LOGICOS
->>>>>>> 942eb5d7104701ca5d7481d379d371ce9620dd63
